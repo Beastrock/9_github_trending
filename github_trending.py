c5aceeda5600fc9af2c4e180cdfe2303ceac3930
Alexey <beastrock@mail.ru>
2016-12-03 20:14:08 +0300

    change variable name

--- github_trending.py
+++ github_trending.py
@@ -4,15 +4,15 @@ from datetime import datetime, timedelta
 
 API_URL = 'https://api.github.com'
 DAYS_INTERVAL = 7
-REPS_AMOUNT = 20
+REPOSITORIES_AMOUNT = 20
 
 
-def get_trending_repositories(reps_amount, days_interval, api_url):
+def get_trending_repositories(repositories_amount, days_interval, api_url):
     days_ago = datetime.today().date() - timedelta(days=days_interval)
     api_method_url = urljoin(api_url, 'search/repositories')
     params = {'q': 'created:>{}'.format(days_ago), "sort": 'stars'}
     repositories_info = requests.get(api_method_url, params=params).json()
-    return repositories_info["items"][:reps_amount]
+    return repositories_info["items"][:repositories_amount]
 
 
 def print_trending_repositories(trending_repositories):
@@ -24,5 +24,5 @@ def print_trending_repositories(trending_repositories):
 
 
 if __name__ == '__main__':
-    repositories = get_trending_repositories(REPS_AMOUNT, DAYS_INTERVAL, API_URL)
+    repositories = get_trending_repositories(REPOSITORIES_AMOUNT, DAYS_INTERVAL, API_URL)
     print_trending_repositories(repositories)
