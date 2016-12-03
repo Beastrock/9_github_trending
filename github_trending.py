245cfb8b7f6b1c060ea69288eaf71627434edb75
Alexey <beastrock@mail.ru>
2016-12-03 20:02:08 +0300

    add global variables

--- github_trending.py
+++ github_trending.py
@@ -3,14 +3,16 @@ from urllib.parse import urljoin
 from datetime import datetime, timedelta
 
 API_URL = 'https://api.github.com'
+DAYS_INTERVAL = 7
+REPS_AMOUNT = 20
 
 
-def get_trending_repositories(amount_of_reps, days_interval=7, api_url=API_URL):
+def get_trending_repositories(reps_amount, days_interval, api_url):
     days_ago = datetime.today().date() - timedelta(days=days_interval)
     api_method_url = urljoin(api_url, 'search/repositories')
     params = {'q': 'created:>{}'.format(days_ago), "sort": 'stars'}
     repositories_info = requests.get(api_method_url, params=params).json()
-    return repositories_info["items"][:amount_of_reps]
+    return repositories_info["items"][:reps_amount]
 
 
 def print_trending_repositories(trending_repositories):
@@ -22,5 +24,5 @@ def print_trending_repositories(trending_repositories):
 
 
 if __name__ == '__main__':
-    repositories = get_trending_repositories(20)
+    repositories = get_trending_repositories(REPS_AMOUNT, DAYS_INTERVAL, API_URL)
     print_trending_repositories(repositories)
